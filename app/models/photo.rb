32a64f2c88136abb81cf161a8f222d9952c3de1b
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-08-14 08:35:11 +0000

    lesson 39 with bugs

---
+++ app/models/photo.rb
@@ -0,0 +1,8 @@
+class Photo < ActiveRecord::Base
+  mount_uploader :picture, PictureUploader
+
+  belongs_to :user
+  belongs_to :places
+end
+
+
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :photos
 
   geocoded_by :address
   after_validation :geocode
@@ -8,4 +9,5 @@ class Place < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
   validates :address, presence: true
   validates :description, presence: true
+
 end
