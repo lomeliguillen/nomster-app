e35201220d0e04752fdae3d29e2581ead35b252e
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-08-19 00:36:32 +0000

    lesson 41 bugged

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "lomeliguillen@gmail.com",
-      subject: "A comment has been added to your place")
+    mail(to: "codetesting3505@gmail.com",
+         subject: "A comment has been added to your place")
   end
 end
