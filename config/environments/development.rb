794a42f503471b7d8977b47ae8f3f6bda95ecd44
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-08-21 06:00:53 +0000

    try to undo some changes to fix comments error, did not work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-   config.action_mailer.delivery_method = :test
+  # config.action_mailer.delivery_method = :smtp
 
   # config.action_mailer.smtp_settings = {
   #   address: "smtp.gmail.com",
@@ -43,7 +43,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  # config.active_storage.service = :local
+  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
