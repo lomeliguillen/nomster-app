c1545637feb8233779e6bb72a9e9baf126a2c8e8
lomeliguillen@gmail.com <lomeliguillen@gmail.com>
2019-08-21 07:11:01 +0000

    lesson 47 still got comment error

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :places do
     resources :comments, :photos, only: :create
   end
+  resources :users, only: :show
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
